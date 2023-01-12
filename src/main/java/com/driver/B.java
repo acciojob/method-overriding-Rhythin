8aba7eee8859b6d1c8e48052f167cf3ad246ae07
Rhythin <105190286+Rhythin@users.noreply.github.com>
2023-01-12 16:13:19 +0530

    solution
    solution

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+public class B extends A{
+
+
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+
+    public String callAmeth() {
+        return super.meth();
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+        B Bobj =new B();
+
+        System.out.println(Bobj.callAmeth());
+        System.out.println(Bobj.meth());
+    }
 }
+
