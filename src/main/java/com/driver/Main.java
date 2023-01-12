9b30537c944964fe31fef7598975acf7174167fa
Rhythin <rhythin13021996@gmail.com>
2023-01-12 22:13:35 +0530

    solution 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,6 @@
 package com.driver;
 
-public class Main {
 
-    public static void main(String args[]){
-        B Bobj =new B();
-
-        System.out.println(Bobj.callAmeth());
-        System.out.println(Bobj.meth());
-    }
-}
 
  class A {
 
@@ -25,6 +17,17 @@ class B extends A{
 
     public String callAmeth() {
         return super.meth();
+    }
+}
+
+
+public class Main {
+
+    public static void main(String args[]){
+        B Bobj =new B();
+
+        System.out.println(Bobj.callAmeth());
+        System.out.println(Bobj.meth());
     }
 }
 
